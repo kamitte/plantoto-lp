72cf7c3e6f779c6ac4f6f800f523d3f8cd7911d4
kamitte <3ma510.96ki@gmail.com>
2025-07-11 17:09:16 +0900

    パラメーターをGAに送る処理を割愛

--- app/ga.tsx
+++ app/ga.tsx
@@ -1,19 +1,17 @@
 "use client";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 const GA_ID = "G-DX815WNMGH";
 
 export default function GoogleAnalytics() {
     const pathname = usePathname();
-    const searchParams = useSearchParams();
 
     useEffect(() => {
-        const url = pathname + (searchParams.toString() ? `?${searchParams}` : "");
         if (typeof window.gtag === "function") {
-            window.gtag("config", GA_ID, { page_path: url });
+            window.gtag("config", GA_ID, { page_path: pathname });
         }
-    }, [pathname, searchParams]);
+    }, [pathname]);
 
     return null;
 }
